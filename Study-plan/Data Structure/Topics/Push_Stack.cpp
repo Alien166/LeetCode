f4c127fa53a3e81cb1e890ec24b48c4eaeac1c6f
Abdeltawab Wagieh <98906055+abdeltawab10@users.noreply.github.com>
2023-03-17 19:38:45 +0200

    Update Push_Stack.cpp

--- Study-plan/Data Structure/Topics/Push_Stack.cpp
+++ Study-plan/Data Structure/Topics/Push_Stack.cpp
@@ -3,11 +3,11 @@ private:
     int top;
     int array[MAX_SIZE];
     stack(){
-        top=-1;
+        top=-1;   // to intialize top
     }
 public:
  void push(int value){
-        if(value>=MAX_SIZE-1){ // 0  -> 99 
+        if(value>=MAX_SIZE-1){ // -1 why?? cuz he starts from  0  -> 99
             cout<<"Stack is fill\n";
         }
         else{
