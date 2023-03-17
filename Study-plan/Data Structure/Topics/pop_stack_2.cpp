728bd194dd945913d24986dcd4fed359a2951126
Abdeltawab Wagieh <98906055+abdeltawab10@users.noreply.github.com>
2023-03-17 20:14:58 +0200

    Delete pop_stack_2.cpp

--- Study-plan/Data Structure/Topics/pop_stack_2.cpp
+++
@@ -1,9 +0,0 @@
- void pop(int &element){    // this function pop to hold value before pop element ;
-     if(is_empty()){
-         cout<<"stack empty on pop";
-     }
-     else{
-         element=array[top];
-         top--;
-     }
-};
