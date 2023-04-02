38cbfa60c75dc9f76f579f68d4522b480c313e92
Abdeltawab Wagieh <98906055+abdeltawab10@users.noreply.github.com>
2023-04-02 22:48:43 +0200

    Create 2300. Successful Pairs of Spells and Potions.cpp

---
+++ Daily_Contest/2300. Successful Pairs of Spells and Potions.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        sort(begin(potions), end(potions));
+        vector<int> result;
+        for (const auto& s: spells) {
+            result.emplace_back(size(potions) - distance(cbegin(potions), lower_bound(cbegin(potions), cend(potions), ceil_divide(success, s))));
+        }
+        return result;
+    }
+
+private:
+    int64_t ceil_divide(int64_t a, int64_t b) {
+        return (a + b - 1) / b;
+    }
+};
