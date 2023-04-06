52c1e65e905d29b01ac149e74758e7e4952760ca
Abdeltawab Wagieh <98906055+abdeltawab10@users.noreply.github.com>
2023-04-06 21:14:33 +0200

    Create 141. Linked List Cycle.cpp

---
+++ Study-plan/Data Structure/problems/141. Linked List Cycle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        set<ListNode*> seen;
+        ListNode* cur = head;
+        
+        while(cur != NULL){
+            // cout << cur->val << " ";
+            if(find(seen.begin(), seen.end(), cur) == seen.end())
+                seen.insert(cur);
+            else
+                return true;
+            cur = cur->next;
+        }
+        cout <<endl;
+        
+        return false;
+    }
+};
