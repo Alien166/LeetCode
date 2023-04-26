a6525c92c319152cfb8de19089d5ae004327733f
Abdeltawab Wagieh <98906055+abdeltawab10@users.noreply.github.com>
2023-04-27 00:12:27 +0200

    Create 206. Reverse Linked List.cpp

---
+++ Study-plan/Data Structure/problems/206. Reverse Linked List.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+
+        ListNode* prev= NULL;
+        ListNode* curr= head ;
+
+        while(curr != NULL ) {
+            ListNode* forward =curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = forward;
+
+        }
+
+          return prev;
+    }
+};
