e41b87d7e838bfd5cf2b62a7f530f0f400f77967
Abdeltawab Wagieh <98906055+abdeltawab10@users.noreply.github.com>
2023-04-28 22:20:28 +0200

    Create get_front-1.cpp

---
+++ Study-plan/Data Structure/topics/Queue/get_front-1.cpp
@@ -0,0 +1,8 @@
+
+  void getFront(int & getFront){
+        if(isEmpty()){
+            cout<<"U have't elements in queue"<<endl;
+        } else {
+            getFront = arr[front];
+        }
+    }
