0e6b57b1e2e64227e45223cff160d8a593c4a7a3
Abdeltawab Wagieh <98906055+abdeltawab10@users.noreply.github.com>
2023-04-29 15:32:02 +0200

    Create main.cpp

---
+++ Study-plan/Data Structure/topics/LinkedQueue/main.cpp
@@ -0,0 +1,17 @@
+
+int main()
+{
+    linkedQueue p;
+    p.enqueue(20);
+    p.enqueue(30);
+    p.enqueue(40);
+    p.dequeue();
+    p.getFront();
+    p.enqueue(50);
+    p.enqueue(60);
+    p.dequeue();
+    p.getRear();
+    p.getSize();
+  
+    return 0;
+}
