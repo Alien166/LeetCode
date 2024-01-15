0e4a3b3003a84c6dd6888a0ebc935d9b32d7c717
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2024-01-15 22:37:59 +0200

    Create find-players-with-zero-or-one-losses.cpp

---
+++ Jun-2024/day-15/find-players-with-zero-or-one-losses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> findWinners(vector<vector<int>>& matches) {
+        vector<vector<int>> ans(2);
+        map<int,int> mp;
+        set<int> st;
+        for(auto it:matches)
+        {
+            mp[it[0]]=0;
+            mp[it[1]]=0;
+        }
+        for(auto it:matches)
+            mp[it[1]]+=1;
+        for(auto it:mp)
+        {
+            if(it.second==0)
+                ans[0].push_back(it.first);
+            if(it.second==1)
+                ans[1].push_back(it.first);
+        }
+        return ans;
+    }
+};
