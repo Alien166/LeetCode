58d3be62c6cd202ab34d58c32a3cf2b3191a73b2
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2024-01-22 20:08:25 +0200

    Create 645. Set Mismatch.cpp

---
+++ Jun-2024/day-22/645. Set Mismatch.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        multiset<int> m1(nums.begin(), nums.end());
+        //if using iota, need to allocate memory for v beforehand
+        vector<int> v(nums.size());
+        iota(v.begin(), v.end(), 1);
+        //build a multiset containing 1~N
+        multiset<int> m2(v.begin(), v.end());
+        
+        vector<int> ans(nums.size());
+        //find duplicate element first
+        vector<int>::iterator it = set_difference(m1.begin(), m1.end(), m2.begin(), m2.end(), ans.begin());
+        //and then find the missing element
+        it = set_difference(m2.begin(), m2.end(), m1.begin(), m1.end(), ans.begin()+1);
+        ans.resize(2);
+        return ans;
+    }
+};
